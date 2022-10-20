12fb58d3ffb42baf2f7ede4f46ced9cf1472e916
MChassanNajib <chassannajib@gmail.com>
2022-10-20 15:09:43 +0700

    feat : [CN] Menambahkan File Splash-Screen

---
+++ lib/Halaman Utama/splash-screen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+// import '../../../untitled1/lib/Copasan/login_screen.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    timerSS();
+  }
+
+  //timer
+  timerSS() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return splashscreen();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 54, 126, 24),
+      body: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 200),
+                child: Image.asset(
+                  "Image/depanlogo.png",
+                  height: 200,
+                  width: 200,
+                ),
+              )
+
+          ]),
+
+    )
+    );
+  }
+
+}
